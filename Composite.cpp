deac339c3dcde4ae347c19299ec7be3f9dcb3dd3
mpm86 <74258852+mpm86@users.noreply.github.com>
2023-10-31 18:18:27 -0400

    Update Composite.cpp

--- Composite.cpp
+++ Composite.cpp
@@ -1,117 +1,97 @@
-#include "Polyhedron.h"
 #include "Composite.h"
 
-//------------------------------------------------------------------------------
 Composite::Composite()
-    :Polyhedron("Composite")
-{
-}
+    :Polyhedron("Composite"), allPolyhedra()
+{}
 
-//------------------------------------------------------------------------------
-/**
- * @todo write this function
- */
 Composite::Composite(const Composite& src)
-    :Polyhedron("Composite")
+    :Polyhedron("Composite"), allPolyhedra()
 {
-    // Perform a deep copy... maybe the _add_ method can help...
+    for (const auto& poly : src.allPolyhedra) {
+        allPolyhedra.push_back(poly->clone());
+        boundingBox.mergeWith(poly->getBoundingBox()); // Ensure bounding box is updated
+    }
 }
 
-//------------------------------------------------------------------------------
-/**
- * @todo write this function
- */
 Composite::~Composite()
 {
-    // Delete each component polyhedra
+    for (auto& poly : allPolyhedra) {
+        delete poly;
+    }
+    allPolyhedra.clear();
 }
 
-//------------------------------------------------------------------------------
-void Composite::read(std::istream& ins){
+void Composite::read(std::istream& ins) {
     int numPolyhedra;
-
     ins >> numPolyhedra;
-
     allPolyhedra.resize(numPolyhedra);
-
     for (int i = 0; i < numPolyhedra; i++) {
         ins >> allPolyhedra[i];
-
         boundingBox.mergeWith(allPolyhedra[i]->getBoundingBox());
     }
 }
 
-//------------------------------------------------------------------------------
-/**
- * @todo write this function
- */
-void Composite::display(std::ostream& outs) const
-{
+void Composite::display(std::ostream& outs) const {
     Polyhedron::display(outs);
-
-    outs << allPolyhedra.size() << " polyhedra" << "\n";
-
-    // Loop through all component polyhedra and
-    // print (display) them
+    outs << allPolyhedra.size() << " polyhedra";
+    for (const auto& poly : allPolyhedra) {
+        outs << std::endl;
+        poly->display(outs);
+    }
 }
 
-//------------------------------------------------------------------------------
-/**
- * @todo write this function
- */
-void Composite::scale(double scalingFactor)
-{
-    // Loop through all polyhedra and scale them
 
-    // Do not forget the bounding box... after the loop
+
+void Composite::scale(double scalingFactor) {
+    for (auto& poly : allPolyhedra) {
+        poly->scale(scalingFactor);
+    }
+    boundingBox.scale(scalingFactor);
 }
 
-//------------------------------------------------------------------------------
-Composite& Composite::operator=(Composite rhs)
-{
+Composite& Composite::operator=(Composite rhs) {
+    // Delete old objects
+    for (auto& poly : allPolyhedra) {
+        delete poly;
+    }
+    allPolyhedra.clear();
+
+    // Swap with the new objects
     swap(*this, rhs);
     return *this;
 }
 
-//------------------------------------------------------------------------------
-Composite::iterator Composite::begin()
-{
-    return allPolyhedra.begin();
+void Composite::add(const Polyhedron* toAdd) {
+    allPolyhedra.push_back(toAdd->clone());
+    boundingBox.mergeWith(toAdd->getBoundingBox());
 }
 
-//------------------------------------------------------------------------------
-Composite::iterator Composite::end()
-{
-    return allPolyhedra.end();
-}
-
-//------------------------------------------------------------------------------
-Composite::const_iterator Composite::begin() const
-{
-    return allPolyhedra.begin();
-}
-
-//------------------------------------------------------------------------------
-Composite::const_iterator Composite::end() const
-{
-    return allPolyhedra.end();
-}
-
-//------------------------------------------------------------------------------
-/**
- * @todo write this function
- */
-void Composite::add(const Polyhedron* toAdd)
-{
-    // Add one new polyhedra and _merge_ its boundingBox with _this->boundingBox_
-}
-
-//------------------------------------------------------------------------------
-void swap(Composite& lhs, Composite& rhs)
-{
+void swap(Composite& lhs, Composite& rhs) {
     using std::swap;
-
     std::swap(lhs.allPolyhedra, rhs.allPolyhedra);
-
     swap(lhs.boundingBox, rhs.boundingBox);
+}
+
+Polyhedron* Composite::clone() const {
+    return new Composite(*this);
+}
+
+int Composite::size() const {
+    return allPolyhedra.size();
+}
+
+Composite::iterator Composite::begin() {
+    return allPolyhedra.begin();
+}
+
+Composite::iterator Composite::end() {
+    return allPolyhedra.end();
+}
+
+Composite::const_iterator Composite::begin() const {
+    return allPolyhedra.begin();
+}
+
+Composite::const_iterator Composite::end() const {
+    return allPolyhedra.end();
 }
