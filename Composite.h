a2b7a06ffdf02fd6667745cf4ccc6580475a70ac
mpm86 <74258852+mpm86@users.noreply.github.com>
2023-10-31 18:18:49 -0400

    Update Composite.h

--- Composite.h
+++ Composite.h
@@ -1,9 +1,8 @@
-#ifndef COMPOSITE_H_INCLUDED
-#define COMPOSITE_H_INCLUDED
-
-#include <vector>
+#ifndef COMPOSITE_H
+#define COMPOSITE_H
 
 #include "Polyhedron.h"
+#include <vector>
 
 class Composite : public Polyhedron {
     public:
@@ -13,99 +12,29 @@ class Composite : public Polyhedron {
         using const_iterator = Collection::const_iterator;
 
     private:
-        /**
-         * Collection of polyhedra of which
-         * this composite polyhedron is composed
-         */
         Collection allPolyhedra;
 
     public:
-
-        /**
-         * Default Constructor
-         */
         Composite();
-
-        /**
-         * Copy Constructor
-         */
         Composite(const Composite& src);
-
-        /**
-         * Destructor
-         */
         virtual ~Composite();
-
-        /**
-         * Assignment Operator
-         */
         Composite& operator=(Composite rhs);
 
-        /**
-         * Return the number of polyhedra that are part of this
-         * Composite object.
-         */
         int size() const;
 
-        // Iterator helpers
         iterator begin();
         iterator end();
-
         const_iterator begin() const;
         const_iterator end() const;
 
-        /**
-         * Add a Polyhedron to the `Composite` collection.
-         *
-         * @post toAdd is cloned and the copy is added.
-         */
         void add(const Polyhedron* toAdd);
 
-        // Polyhedron Interface
+        virtual Polyhedron* clone() const override;
+        virtual void read(std::istream& ins) override;
+        virtual void display(std::ostream& outs) const override;
+        virtual void scale(double scalingFactor) override;
 
-        /**
-         * Copy Constructor Wrapper
-         */
-        virtual Polyhedron* clone() const;
-
-        /**
-         * Read all component polyhedra
-         *
-         * @pre numPolyhedra == 0
-         */
-        virtual void read(std::istream& ins);
-
-        /**
-         * Print all polyhedra
-         */
-        virtual void display(std::ostream& outs) const;
-
-        /**
-         * Scale all polyhedra
-         */
-        virtual void scale(double scalingFactor);
-
-        /**
-         * Swap the contents of two `Composite`s
-         * <p>
-         * I am using a friend function here and only here (under protest)
-         */
-        friend
-        void swap(Composite& lhs, Composite& rhs);
+        friend void swap(Composite& lhs, Composite& rhs);
 };
 
-//------------------------------------------------------------------------------
-inline
-int Composite::size() const
-{
-    return this->allPolyhedra.size();
-}
-
-//------------------------------------------------------------------------------
-inline
-Polyhedron* Composite::clone() const
-{
-    return new Composite(*this);
-}
-
-#endif
+#endif // COMPOSITE_H
